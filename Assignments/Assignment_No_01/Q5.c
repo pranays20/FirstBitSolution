99c429528216db1aa3ee28ea6ea7428b7ce5051e
Pranay Shinde <shindepranay039@gmail.com>
2025-09-29 17:13:49 +0530

    Add file

--- Assignments/Assignment_No_01/Q5.c
+++ Assignments/Assignment_No_01/Q5.c
@@ -2,5 +2,8 @@ void main()
 {
 	//5.Write a C program to input five numbers and find their average.
     int num1 = 10, num2 = 20, num3 = 30, num4 = 40, num5 = 50;
-	float sum, average;	
+	float sum, average;
+		sum = num1 + num2 + num3 + num4 + num5;
+	average = sum / 5;
+	printf("Average = %.2f", average);	
 }
